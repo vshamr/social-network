b261b56d597645297b65e3fb4630fffeadf4fc3f
vshamr <vero94@mail.ru>
2021-07-03 16:58:46 +0300

    change status (setState) 70

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
+import ProfileStatus from "./ProfileInfo/ProfileStatus";
 
 const Profile = (props) => {
     return (
         <div>
             <ProfileInfo profile={props.profile}/>
             <MyPostsContainer />
+
         </div>
     )
 }
